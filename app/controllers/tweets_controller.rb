9447e620411849f6afd1ea221a7bd786f46ff828
silverskg <chokichoki_gm@i.softbank.jp>
2020-10-17 19:52:33 +0900

    createアクション定義

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,17 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all
   end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  private
+  def tweet_params
+    params.permit(:name, :image, :text)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :tweets, only: :index
+  resources :tweets, only: [:index, :new, :create]
 end
