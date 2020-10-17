22cf51f56398c61ef45cfc7b9bff91afd81f845b
silverskg <chokichoki_gm@i.softbank.jp>
2020-10-17 20:33:59 +0900

    削除機能実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -12,6 +12,11 @@ class TweetsController < ApplicationController
     Tweet.create(tweet_params)
   end
 
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+  end
+
   private
   def tweet_params
     params.require(:tweet).permit(:name, :image, :text)
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :tweets, only: [:index, :new, :create]
+  resources :tweets, only: [:index, :new, :create, :destroy]
 end
