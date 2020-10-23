43dc0ad9ae13da30c349f5a11c2e0a1b49d7e703
silverskg <chokichoki_gm@i.softbank.jp>
2020-10-23 20:39:17 +0900

    コメントコントローラー作成

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,5 @@
 class Tweet < ApplicationRecord
   validates :text, presence: true
   belongs_to :user
+  has_many :comments
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   root to: 'tweets#index'
   resources :tweets
   resources :users, only: [:show, :index]
+  resources :tweets do 
+    resources :comments, only: :create
+  end
 end
