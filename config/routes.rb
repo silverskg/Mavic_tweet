768f590ac4e3ee8c30d4b89cf02dfd689345872f
silverskg <chokichoki_gm@i.softbank.jp>
2020-10-24 21:49:15 +0900

    検索画面用のviewファイル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'tweets#index'
-  resources :tweets
   resources :users, only: [:show, :index]
   resources :tweets do 
     resources :comments, only: :create
+    collection do 
+      get 'search'
+    end
   end
 end
