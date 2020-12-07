9c2f3f10af33d7e866f16420fe4a80d93da8f206
silverskg <chokichoki_gm@i.softbank.jp>
2020-12-07 15:46:31 +0900

    ルーティング記述

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :index]
   resources :tweets do 
     resources :comments, only: :create
+    resources :likes only: [:create, :destroy]
     collection do 
       get 'search'
     end
