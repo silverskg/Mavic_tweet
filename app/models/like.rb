f07a7b20b0d4de1b1a1088a306c0b556b9153e88
silverskg <chokichoki_gm@i.softbank.jp>
2020-12-07 15:17:28 +0900

    モデル間のアソシエーション定義

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,4 +2,8 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :tweet
 
+  validates :user_id, uniqueness: {
+    scope: :post_id,
+    message: "は同じ投稿に２回以上いいねできません"
+  }
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,7 +2,7 @@ class Tweet < ApplicationRecord
   validates :text, presence: true
   belongs_to :user
   has_many :comments
-
+  has_many :likes, dependent: :destroy
   
   def self.search(search)
     return Tweet.all  unless search
